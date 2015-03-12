6fc25332a2b3bb1e26c6af15e5956ed16c9fe98f
Dmytrii Sinko <t4ec@users.noreply.github.com>
2015-03-12 19:55:36 +0200

    Add vagrant files

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+hosts = {
+  "web" => "192.168.33.33",
+}
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  hosts.each do |name, ip|
+    config.vm.define name do |machine|
+      machine.vm.box = "precise64"
+      machine.vm.box_url = "http://files.vagrantup.com/precise64.box"
+      machine.vm.hostname = "%s.local" % name
+      machine.vm.network :private_network, ip: ip
+
+      machine.vm.synced_folder ".", "/home/vagrant/"
+
+      machine.vm.provision :puppet do |puppet|
+          puppet.manifests_path = "puppet/manifests"
+          puppet.manifest_file  = "vagrant.pp"
+      end
+    end
+  end
+end
---
+++ puppet/manifests/vagrant.pp
@@ -0,0 +1,50 @@
+$PROJ_DIR = "/home/vagrant/"
+
+Exec {
+    path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
+}
+
+class dev {
+
+    class {
+        init:
+    }
+}
+
+include dev
+
+class init {
+
+    group { "puppet":
+        ensure => "present",
+    }
+
+    # Update the system
+    exec { "update-apt":
+        command => "sudo apt-get update",
+    }
+
+    # Install python packages we need
+    package {
+        ["python", "python-dev", "python-pip"]:
+        ensure => installed,
+        require => Exec['update-apt'] # The system update needs to run first
+    }
+
+    # Install other packages we need
+    package {
+        ["vim", "screen"]:
+        ensure => installed,
+        require => Exec['update-apt'] # The system update needs to run first
+    }
+
+    # Install project dependecies from pip
+    exec { "pip-install-requirements":
+        command => "sudo /usr/bin/pip install -r $PROJ_DIR/requirements.txt",
+        tries => 2,
+        timeout => 600, # Too long, but this can take awhile
+        require => Package['python-pip', 'python-dev'], # The package dependecies needs to run first
+        logoutput => on_failure,
+    }
+
+}
