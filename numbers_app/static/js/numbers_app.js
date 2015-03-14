5abbb651286dd81cc3d0c24fad1f73cac9987791
Dmytrii Sinko <t4ec@users.noreply.github.com>
2015-03-14 11:14:51 +0200

    Implement "Game over" behavior
    * We check how many of number we have left
    * If number is <= 0 we show "Game over" alert and reload
      page
    * If number is > 0 we just remove number

--- numbers_app/static/js/numbers_app.js
+++ numbers_app/static/js/numbers_app.js
@@ -6,7 +6,7 @@ angular.module('numbersApp', [])
       var counter = array.length;
       var temp;
       var index;
-
+      console.log(array);
       // While there are elements in the array
       while (counter > 0) {
           counter--;
@@ -26,22 +26,47 @@ angular.module('numbersApp', [])
 
     function gen_rand_array_with_duplicates(size, max_value, duplicates_num) {
       var array = [];
+      var array_count = {};
       // Fill first part of array with random values
       for (i=0; i < size - duplicates_num; i++) {
         array[i] = Math.round(Math.random() * max_value);
+        if (array[i] in array_count) {
+          array_count[array[i]]++;
+          console.log("+1");
+          continue;
+        }
+        array_count[array[i]] = 1;
       }
 
       // Fill second part of array with duplicates
       for (i = size - duplicates_num; i < size; i++) {
-        rand_index = Math.round(Math.random() * (size - duplicates_num));
+        rand_index = Math.round(Math.random() * (size - duplicates_num - 1));
         array[i] = array[rand_index];
+        if (array[i] in array_count) {
+          array_count[array[i]]++;
+        }
       }
 
-      return shuffle(array);
+      return [shuffle(array), array_count];
     }
 
-    var duplicates_num = 1 + Math.round(Math.random() * 8);
-    $scope.array = gen_rand_array_with_duplicates(10, 200, duplicates_num);
+    var duplicates_num = 1 + Math.round(Math.random() * 4);
+    gen_data = gen_rand_array_with_duplicates(10, 200, duplicates_num);
+    $scope.array = gen_data[0];
+    var array_count = gen_data[1];
+    console.log(array_count);
+
+    $scope.remove = function(index) {
+      var value = $scope.array[index];
+      $scope.array.splice(index, 1);
+      array_count[value]--;
+
+
+      if (array_count[value] <= 0) {
+        $scope.array = [];
+        if(!window.alert("Game over")){window.location.reload();}
+      }
+    }
 
     // $scope.todos = [
     //   {text:'learn angular', done:true},
