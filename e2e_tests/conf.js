1060655e2c68fad10e3bc22c3adbceba52cd7649
Dmytrii Sinko <t4ec@users.noreply.github.com>
2015-03-15 20:11:33 +0200

    Do not use phantomjs due to random crashes

--- e2e_tests/conf.js
+++ e2e_tests/conf.js
@@ -1,8 +1,4 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['numbers-spec.js'],
-    capabilities: {
-      browserName: 'phantomjs',
-      'phantomjs.binary.path': require('phantomjs').path
-    },
 };
